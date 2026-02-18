416dd95da7f31fdedb4d8114b3f5db9b0f72aa5a
prranaaman464-star <pr.ranaaman464@gmail.com>
2026-02-18 22:47:59 +0530

    bug-fix

--- routes/web.php
+++ routes/web.php
@@ -60,28 +60,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     });
 });
 
-Route::middleware(['auth', 'verified', RedirectIfNoOrganization::class])->prefix('inventory')->group(function () {
-    Route::get('items', function () {
-        return Inertia::render('inventory/Items');
-    })->name('inventory.items');
-    Route::get('items/create', function () {
-        return Inertia::render('inventory/Items/Create');
-    })->name('inventory.items.create');
-    Route::post('items', function () {
-        return Inertia::render('inventory/Items/Store');
-    })->name('inventory.items.store');
-    Route::get('items/{item}', function () {
-        return Inertia::render('inventory/Items/Show');
-    })->name('inventory.items.show');
-    Route::get('items/{item}/edit', function () {
-        return Inertia::render('inventory/Items/Edit');
-    })->name('inventory.items.edit');
-    Route::put('items/{item}', function () {
-        return Inertia::render('inventory/Items/Update');
-    })->name('inventory.items.update');
-    Route::delete('items/{item}', function () {
-        return Inertia::render('inventory/Items/Destroy');
-    })->name('inventory.items.destroy');
 Route::middleware(['auth', 'verified'])->prefix('inventory')->group(function () {
     Route::get('items', [InventoryItemsController::class, 'index'])->name('inventory.items');
     Route::get('items/create', [InventoryItemsController::class, 'create'])->name('inventory.items.create');
